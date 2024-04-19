f60cf3d4e517969c9434748f132dabed45b41a25
Gaurav sharma <138372654+Grvslash02@users.noreply.github.com>
2024-04-19 22:35:38 +0530

    Update userRoutes.js

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,1 +1,21 @@
-Here are user routes
+const express=require('express');
+const userController=require('./../controllers/usercontroller');
+const authController=require('./../controllers/authController');
+const userRouter= express.Router();
+
+userRouter.post('/signup',authController.signup);
+userRouter.post('/login',authController.login);
+userRouter.post('/forgotPassword',authController.forgotPassword);
+userRouter.patch('/resetPassword/:token',authController.resetPassword );
+
+
+userRouter.route('/')
+    .get(userController.getAllUsers)
+    .post(userController.addNewUser)
+userRouter.route('/:id?')
+    .get(userController.getUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser)
+
+module.exports=userRouter;
+
